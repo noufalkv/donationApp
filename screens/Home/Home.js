53303cb2bf908a6fa389abc7dfced6d3020c9f32
Noufal <noufal@Noufals-MacBook-Pro.local>
2025-02-08 20:10:10 +0400

    Button component

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -2,6 +2,7 @@ import {SafeAreaView, View} from 'react-native';
 import {Text} from 'react-native-gesture-handler';
 import globalStyle from '../../assets/styles/globalStyle';
 import Header from '../../components/Header/Header';
+import Button from '../../components/Button/Button';
 
 const Home = () => {
   return (
@@ -12,6 +13,14 @@ const Home = () => {
       <Header title={'Azzahri A.'} type={1} />
       <Header title={'Azzahri A.'} type={2} />
       <Header title={'Azzahri A.'} type={3} />
+
+      <Button
+        title={'Donate'}
+        onPress={() => {
+          console.log('You just pressed me!');
+        }}
+      />
+      <Button title={'Donate'} isDisabled={true} />
     </SafeAreaView>
   );
 };
