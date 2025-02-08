457e1b1b4732682a243f9494dfdc6d082620b754
Noufal <noufal@Noufals-MacBook-Pro.local>
2025-02-08 19:26:30 +0400

    Added Navigation

--- App.tsx
+++ App.tsx
@@ -6,17 +6,14 @@
  */
 
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
-import {getFontFamily} from './assets/fonts/helper';
+import {NavigationContainer} from '@react-navigation/native';
+import MainNavigation from './navigation/MainNavigation';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <Text style={{fontSize: 70, fontFamily: getFontFamily('Inter', '900')}}>
-        {' '}
-        Hello World
-      </Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      <MainNavigation />
+    </NavigationContainer>
   );
 };
 
---
+++ screens/Home/Home.js
@@ -0,0 +1,13 @@
+import {SafeAreaView, View} from 'react-native';
+import {Text} from 'react-native-gesture-handler';
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Home</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+export default Home;
