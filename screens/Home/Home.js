bcbe2dff12e9a7c890e697b94b600a20c7f6f137
Noufal <noufal@Noufals-MacBook-Pro.local>
2025-02-08 20:05:28 +0400

    Header component

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,12 +1,17 @@
 import {SafeAreaView, View} from 'react-native';
 import {Text} from 'react-native-gesture-handler';
+import globalStyle from '../../assets/styles/globalStyle';
+import Header from '../../components/Header/Header';
 
 const Home = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex1]}>
       <View>
         <Text>Home</Text>
       </View>
+      <Header title={'Azzahri A.'} type={1} />
+      <Header title={'Azzahri A.'} type={2} />
+      <Header title={'Azzahri A.'} type={3} />
     </SafeAreaView>
   );
 };
